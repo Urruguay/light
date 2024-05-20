3db9aec04e8f30942fe5ad969c333f856caaa2bc
Urruguay <vikuliashurduk@gmail.com>
2024-05-20 19:13:18 +0300

    add

---
+++ 05.20.2024/Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle<int> triangle = new Triangle<int>(0, 0, 0);
+            triangle.Info();
+            Console.WriteLine($"Площадь - {triangle.Area()}\n");
+
+            Triangle<uint> triangle1 = new Triangle<uint>(2u, 3u, 4u);
+            triangle1.Info();
+            Console.WriteLine($"Площадь - {triangle1.Area()}\n");
+
+            Triangle<double> triangle2 = new Triangle<double>(4.5, 5.5, 6.5);
+            triangle2.Info();
+            Console.WriteLine($"Площадь - {triangle2.Area():f2}\n");
+
+            Console.Read();
+        }
+    }
+}
---
+++ 05.20.2024/Task_2/Triangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Triangle<K>
+    {
+        public K a; public K b; public K c;
+
+        public K A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public K B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public K C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        public Triangle() { }
+        public Triangle(K a, K b, K c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        
+        public dynamic Area()
+        {
+            dynamic a1 = A;
+            dynamic b1 = B;
+            dynamic c1 = C;
+            dynamic p = (a1 + b1 + c1) / 2;
+            if (a1 == 0 && b1 == 0 && c1 == 0)
+            {
+                Console.WriteLine("Такого не существует:");
+            }
+             return Math.Sqrt(p * (p - a1) * (p - b1) * (p - c1));
+            
+            
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Сторона a = {A}\nСторона b = {B}\nСторона c = {C}");
+        }
+    }
+}
