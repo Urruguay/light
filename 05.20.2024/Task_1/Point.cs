8202fbacf26c5af893631f597234cd4d52e8dfd7
Urruguay <vikuliashurduk@gmail.com>
2024-05-20 19:23:13 +0300

    add

---
+++ 05.20.2024/Task_1/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Point<T>
+    {
+        private T x;
+        private T y;
+
+        public Point() { }
+        public Point(T x, T y) 
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public T X
+        { get { return x; } set { value = x; } }
+        public T Y
+        { get { return y; } set { value = y; } }
+
+        public void Info()
+        {
+            Console.WriteLine($"Значение x = {X}\nЗначение y = {Y}\n");
+        }
+    }
+}
---
+++ 05.20.2024/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> num = new Point<int>(2, 3);
+            num.Info();
+
+            Point<uint> num2 = new Point<uint>(3u, 4u);
+            num2.Info();
+
+            Point<double> num3 = new Point<double>(4.2, 5.1);
+            num3.Info();
+
+            Console.Read();
+        }
+    }
+}
